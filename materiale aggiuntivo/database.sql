1b64f138492ec5b1dcd2ac8371176fa9214441d2
LuiWet01 <61627200+LuiWet01@users.noreply.github.com>
2020-03-08 20:24:25 +0100

    Create database.sql

---
+++ materiale aggiuntivo/database.sql
@@ -0,0 +1,77 @@
+use my_gruppo1;
+
+create table user
+(
+
+	CodU char(5) not null,
+	NomeU varchar(30) not null,
+	CognomeU varchar(30) not null, 
+	Email varchar(30) not null,
+	Password varchar(512) not null,
+	Primary Key(CodU)
+
+)Engine=INNODB;
+
+create table GenereFilm
+(
+
+	CodGF char(5) not null,
+	NomeGF varchar(30) not null,
+	Primary Key(CodGF)
+
+)Engine=INNODB;
+
+create table RegistaFilm
+(
+
+	CodRF char(5) not null,
+	NomeRF varchar(30) not null,
+	CognomeRF varchar(30) not null, 
+	Primary Key(CodRF)
+
+)Engine=INNODB;
+
+create table Ordine
+(
+
+	CodO char(5) not null,
+	DataO date not null,
+	FK_CodU char(5) not null, 
+	FOREIGN KEY (FK_CodU) REFERENCES user(CodU)
+	Primary Key(CodO)
+
+)Engine=INNODB;
+
+
+create table Elenco_Film
+(
+
+	CodEF char(5) not null,
+	Titolo varchar(50) not null,
+	Descrizione varchar(200) not null,
+	Locandina varchar(200) not null,
+	Durata int not null,
+
+	FK_CodGF char(5) not null,
+	FK_CodRF char(5) not null,
+	FOREIGN KEY (FK_CodGF) REFERENCES GenereFilm(CodGF)
+	FOREIGN KEY (FK_CodRF) REFERENCES RegistaFilm(CodRF)
+
+	Primary Key(CodEF)
+
+)Engine=INNODB;
+
+create table Spettacoli
+(
+
+	CodS char(5) not null,
+	Sala char(1) not null,
+	Orario not null,
+	Costo float not null,
+
+	FK_CodEF char(5) not null,
+	FOREIGN KEY (FK_CodEF) REFERENCES Elenco_Film(CodEF)
+
+	Primary Key(CodS)
+
+)Engine=INNODB;
